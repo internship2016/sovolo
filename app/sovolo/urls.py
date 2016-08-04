50a42e2c0aebc4d3bbaf196e3752512416cac99d
Shin Kojima <shin@kojima.org>
2016-08-04 15:15:01 -0700

    Just for confirm current Python version

--- app/sovolo/urls.py
+++ app/sovolo/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import sys
+raise Exception('VERSION', sys.version)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
