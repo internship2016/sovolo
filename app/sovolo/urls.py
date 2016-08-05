c9fa4ba11ca262117e34453686e8b61b17ce1a08
栗田啓大 <kris337jbn@yahoo.co.jp>
2016-08-05 11:20:50 -0700

    Modelごとにurlを管理できるようにしました

--- app/sovolo/urls.py
+++ app/sovolo/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^event/', include('event.urls')),
+    url(r'^group/', include('group.urls')),
+    url(r'^tag/', include('tag.urls')),
+    url(r'^user/', include('user.urls')),
 ]
