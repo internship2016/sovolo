a0d2ec81b49fc4b22d455b3e899db220067beb1e
Takahiro Sugiura <tksg2333@gmail.com>
2016-08-08 15:55:14 -0700

    settingファイルの修正

--- app/sovolo/settings.py
+++ app/sovolo/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            '/templates'
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
