4f3e2c781087f579892d2221d81a19e61d1403f7
swdrsk <sawaryou325@gmail.com>
2016-08-08 15:41:29 -0700

    ログイン・登録画面の追加

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    url(r'^(?P<user_id>[0-9]+)/$', views.IndexView.view, name='userpage'),
-    #url(r'^$', views.IndexView.view,name='userpage'),
+    url(r'^(?P<user_id>[0-9]+)/$', views.IndexView.view, name='user_page'),
+    url(r'^login/$', views.login_view,name='login'),
+    url(r'^register/$', views.register_view,name='register')
 ]
