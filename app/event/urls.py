d62d8da992e5948eeba294810a4b58fde9cecb88
swdrsk <sawaryou325@gmail.com>
2016-08-08 16:13:35 -0700

    イベント作成編集画面の追加

--- app/event/urls.py
+++ app/event/urls.py
@@ -5,4 +5,6 @@ app_name='event'
 
 urlpatterns = [
     url(r'^(?P<event_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<event_id>[0-9]+)/edit/$', views.edit, name='edit'),
+    url(r'^add/$', views.add, name='add'),
 ]
--- app/event/views.py
+++ app/event/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def detail(request, event_id):
     return render(request, 'event/detail.html', {'event_id':event_id})
+
+def edit(request,event_id):
+    data ={
+        'event_id':event_id
+    }
+    return render(request, 'event/edit.html', data)
+
+def add(request):
+    return render(request, 'event/add.html')
