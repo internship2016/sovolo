68eb4c7ce98a61014fed8c08b4b176a26db46fbd
Takahiro Sugiura <tksg2333@gmail.com>
2016-08-08 15:55:51 -0700

    TOPページのテンプレートを作成

--- app/sovolo/views.py
+++ app/sovolo/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('This is top page.')
+    return render(request, 'top.html')
