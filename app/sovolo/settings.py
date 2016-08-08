e9a740808cb0551b6f711b6a493bd646298c3a66
tntktnx <you@example.com>
2016-08-09 03:22:21 +0900

    ロケール・タイムゾーン設定を日本のものに変更

--- app/sovolo/settings.py
+++ app/sovolo/settings.py
@@ -107,14 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
