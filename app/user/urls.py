5bf3b4e4fcbdf1c26cabd00f979fa1a222294c9a
栗田啓大 <kris337jbn@yahoo.co.jp>
2016-08-09 17:05:10 -0700

    Userの詳細、削除、編集ページを作成しました

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,7 +4,13 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    url(r'^(?P<user_id>[0-9]+)/$', views.IndexView.view, name='user_page'),
+
     url(r'^login/$', views.login_view,name='login'),
-    url(r'^register/$', views.register_view,name='register')
+    url(r'^register/$', views.register_view,name='register'),
+    # user/1/detail
+    url(r'^(?P<pk>[0-9]+)/$', views.UserDetailView.as_view(), name='detail'),
+    # user/1/edit/
+    url(r'^(?P<pk>[0-9]+)/edit/$', views.UserEditView.as_view(), name='edit'),
+    # user/1/delete
+    url(r'^(?P<pk>[0-9]+)/delete/$', views.UserDeleteView.as_view(), name='delete'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
+from django.core.urlresolvers import reverse_lazy
+from .models import User
+
+from django.utils import timezone
 
 # Create your views here.
 
@@ -16,3 +22,24 @@ def login_view(request):
 
 def register_view(request):
     return render(request, "user/register_page.html")
+
+
+
+class UserDetailView(DetailView):
+    template_name = 'user/detail.html'
+    model = User
+    context_object_name = 'user'
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+class UserEditView(UpdateView):
+    model = User
+    fields = ['nickname']
+    template_name = 'user/edit.html'
+
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('event:index')
+    template_name = 'user/check_delete.html'
