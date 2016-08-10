6d9a0b6513e6c8832cc46729fc7bc9258675b5d1
Shin Kojima <shin@kojima.org>
2016-08-10 16:20:56 -0700

    PostgreSQLを使うようにした

--- app/sovolo/settings.py
+++ app/sovolo/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'sovolo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sovolo',
+        'USER': 'sovolo_admin',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
