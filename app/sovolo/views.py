0d9e4b3e4cd8b81f0ec0a7cea9e180b6c40d2f62
栗田啓大 <kris337jbn@yahoo.co.jp>
2016-08-19 16:47:36 -0700

    タグの検索をチェックボックスにしました

--- app/sovolo/views.py
+++ app/sovolo/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from event.models import Event
-
+from tag.models import Tag
 
 def index(request):
+    all_tags = Tag.objects.all()
+    context = {'all_tags': all_tags}
+
     if request.user.is_anonymous():
         new_events = Event.objects.all().order_by('-created')[:20]
-        context = {'new_events': new_events}
-
+        context['new_events'] = new_events
         return render(request, 'top_anonymous.html', context)
     else:
-        return render(request, 'top.html')
+        return render(request, 'top.html', context)
