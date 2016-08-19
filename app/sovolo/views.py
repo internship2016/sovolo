65258e17fba5495f4ef1cf45469d8201073f075c
Takahiro Sugiura <tksg2333@gmail.com>
2016-08-18 18:12:09 -0700

    Fix#52 ログインしていない時のトップページを作成

--- app/sovolo/views.py
+++ app/sovolo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from event.models import Event
 
 
 def index(request):
-    return render(request, 'top.html')
+    if request.user.is_anonymous():
+        new_events = Event.objects.all().order_by('-created')[:20]
+        context = {'new_events': new_events}
+
+        return render(request, 'top_anonymous.html', context)
+    else:
+        return render(request, 'top.html')
