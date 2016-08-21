bc913eac6a321031999adc631940942a8a00f6b9
Shin Kojima <shin@kojima.org>
2016-08-21 16:41:02 -0700

    インデントを修正しました

--- app/static/script/myjs.js
+++ app/static/script/myjs.js
@@ -2,52 +2,52 @@
   var $ = window.jQuery;
   var moment = window.moment;
 
-$('.dropdown-toggle').dropdown();
+  $('.dropdown-toggle').dropdown();
 
-moment.locale('ja', { week: { dow: 1 } });
+  moment.locale('ja', { week: { dow: 1 } });
 
-var DateFormat = 'YYYY-MM-DD';
-$(function () {
+  var DateFormat = 'YYYY-MM-DD';
+  $(function () {
     $('#searchform-date').datetimepicker({
-        inline: true,
-        locale: moment.locale('ja'),
-        format: DateFormat
+      inline: true,
+      locale: moment.locale('ja'),
+      format: DateFormat
     });
 
     $('#order-results').change(function() {
-        var Url = window.location.href;
-        switch(this.value){
-            case "開始日(昇順）":
-                var desc = "asc";
-                break;
-            case "開始日(降順)":
-                var desc = "desc";
-                break;
-            default:
-                var desc = "";
-                break;
-        }
-        var regex = /\b(order_by=start_time-)[^&]*/;
-        if(regex.test(Url)){
-            var newUrl = Url.replace(regex, '$1' + desc);
-        } else{
-            var newUrl = Url + "&order_by=start_time-" + desc;
-        }
-        newUrl = newUrl.replace(/\bpage=\d+/,"page=1");
-        window.location.replace(newUrl);
+      var Url = window.location.href;
+      switch(this.value){
+        case "開始日(昇順）":
+          var desc = "asc";
+          break;
+        case "開始日(降順)":
+          var desc = "desc";
+          break;
+        default:
+          var desc = "";
+          break;
+      }
+      var regex = /\b(order_by=start_time-)[^&]*/;
+      if(regex.test(Url)){
+        var newUrl = Url.replace(regex, '$1' + desc);
+      } else{
+        var newUrl = Url + "&order_by=start_time-" + desc;
+      }
+      newUrl = newUrl.replace(/\bpage=\d+/,"page=1");
+      window.location.replace(newUrl);
     });
 
     $('#result-number').change(function() {
-        var Url = window.location.href;
-        num = this.value
-        var regex = /\b(numperpage=)[^&]*/;
-        if(regex.test(Url)){
-            var newUrl = Url.replace(regex, '$1' + num);
-        } else{
-            var newUrl = Url + "&numperpage=" + num;
-        }
-        window.location.replace(newUrl)
+      var Url = window.location.href;
+      num = this.value
+      var regex = /\b(numperpage=)[^&]*/;
+      if(regex.test(Url)){
+        var newUrl = Url.replace(regex, '$1' + num);
+      } else{
+        var newUrl = Url + "&numperpage=" + num;
+      }
+      window.location.replace(newUrl)
     });
-});
+  });
 
 })(this);
