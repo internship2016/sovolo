5dfee5b5fe3b1b38a8336c5c50d6c9855cc48c96
Shin Kojima <shin@kojima.org>
2017-08-21 16:44:20 -0700

    [COSME] F401 'django.core.files.uploadedfile.InMemoryUploadedFile' imported but unused

--- app/event/models.py
+++ app/event/models.py
@@ -8,7 +8,6 @@ from django.db.models import Q
 from django.utils import timezone
 from tag.models import Tag
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
 import os
 import math
