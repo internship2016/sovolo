4f8b145b9647f59924cddcee6d10aee3f5cfb68c
Shin Kojima <shin@kojima.org>
2017-08-21 22:02:14 -0700

    [COSME] E203 whitespace before ':'

--- app/event/api.py
+++ app/event/api.py
@@ -16,33 +16,33 @@ def event_filter(request, event_kind, *args, **kwargs):
             res_obj = {'filtered_events': []}
             for event in events:
                 res_obj['filtered_events'].append({
-                    'id' : event.id,
-                    'name' : event.name,
-                    'start_time' : event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
-                    'end_time' : event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
-                    'place' : event.meeting_place,
-                    'img' : event.get_image_url(),
-                    'status' : event.get_status()
+                    'id': event.id,
+                    'name': event.name,
+                    'start_time': event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    'end_time': event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    'place': event.meeting_place,
+                    'img': event.get_image_url(),
+                    'status': event.get_status()
                     })
             return JsonResponse(res_obj)
         events = {
-                'future_participating_events' : request.user.get_future_participating_events,
-                'new_group_events' : request.user.get_new_group_events,
-                'new_region_events' : request.user.get_new_region_events,
-                'new_tag_events' : request.user.get_new_tag_events,
-                'new_events' : new_events
+                'future_participating_events': request.user.get_future_participating_events,
+                'new_group_events': request.user.get_new_group_events,
+                'new_region_events': request.user.get_new_region_events,
+                'new_tag_events': request.user.get_new_tag_events,
+                'new_events': new_events
                 }
         if event_kind in events.keys():
             res_obj = {'filtered_events':[]}
             for event in events[event_kind]()[:10]:
                 res_obj['filtered_events'].append({
-                    'id' : event.id,
-                    'name' : event.name,
-                    'start_time' : event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
-                    'end_time' : event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
-                    'place' : event.meeting_place,
-                    'img' : event.get_image_url(),
-                    'status' : event.get_status()
+                    'id': event.id,
+                    'name': event.name,
+                    'start_time': event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    'end_time': event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    'place': event.meeting_place,
+                    'img': event.get_image_url(),
+                    'status': event.get_status()
                     })
             return JsonResponse(res_obj)
     else:
@@ -50,7 +50,7 @@ def event_filter(request, event_kind, *args, **kwargs):
 def event_range_search(request,  *args, **kwargs):
     if request.method == 'POST':
         res = {'events_in_range':[]}
-        # json_data = json.loads('{"ne_lat" : 0, "sw_lat" :200, "ne_lng" : 0, "sw_lng" : 200 }')
+        # json_data = json.loads('{"ne_lat": 0, "sw_lat":200, "ne_lng": 0, "sw_lng": 200 }')
         range_value = dict(request.POST)
         keys = ['ne_lat','sw_lat','ne_lng','sw_lng']
         for key in keys:
@@ -58,14 +58,14 @@ def event_range_search(request,  *args, **kwargs):
 
         for event in Event.get_events_in_range(range_value['ne_lat'], range_value['sw_lat'], range_value['ne_lng'], range_value['sw_lng']):
             res['events_in_range'].append({
-                'id' : event.id,
-                'name' : event.name,
-                'start_time' : event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
-                'end_time' : event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
-                'place' : event.meeting_place,
-                'longitude' : event.longitude,
+                'id': event.id,
+                'name': event.name,
+                'start_time': event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                'end_time': event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                'place': event.meeting_place,
+                'longitude': event.longitude,
                 'latitude': event.latitude,
-                'img' : event.get_image_url(),
-                'status' : event.get_status()
+                'img': event.get_image_url(),
+                'status': event.get_status()
                 })
         return JsonResponse(res)
