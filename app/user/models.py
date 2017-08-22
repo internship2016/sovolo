9d303bfef8a83e14a3d462fbe951742b0f476508
Shin Kojima <shin@kojima.org>
2017-08-21 22:39:26 -0700

    [COSME] F401 'django.contrib.admin' imported but unused

--- app/user/models.py
+++ app/user/models.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from django.db import models
-from django.contrib import admin
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.urlresolvers import reverse
 from django.conf import settings
