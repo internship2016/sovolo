bf732987361384d70c306d330fd0fd941c054a0e
Shin Kojima <shin@kojima.org>
2017-08-21 22:04:13 -0700

    [COSME] Fix white spaces

--- app/event/api.py
+++ app/event/api.py
@@ -23,17 +23,17 @@ def event_filter(request, event_kind, *args, **kwargs):
                     'place': event.meeting_place,
                     'img': event.get_image_url(),
                     'status': event.get_status()
-                    })
+                })
             return JsonResponse(res_obj)
         events = {
-                'future_participating_events': request.user.get_future_participating_events,
-                'new_group_events': request.user.get_new_group_events,
-                'new_region_events': request.user.get_new_region_events,
-                'new_tag_events': request.user.get_new_tag_events,
-                'new_events': new_events
-                }
+            'future_participating_events': request.user.get_future_participating_events,
+            'new_group_events': request.user.get_new_group_events,
+            'new_region_events': request.user.get_new_region_events,
+            'new_tag_events': request.user.get_new_tag_events,
+            'new_events': new_events
+        }
         if event_kind in events.keys():
-            res_obj = {'filtered_events':[]}
+            res_obj = {'filtered_events': []}
             for event in events[event_kind]()[:10]:
                 res_obj['filtered_events'].append({
                     'id': event.id,
@@ -43,20 +43,23 @@ def event_filter(request, event_kind, *args, **kwargs):
                     'place': event.meeting_place,
                     'img': event.get_image_url(),
                     'status': event.get_status()
-                    })
+                })
             return JsonResponse(res_obj)
     else:
         return JsonResponse(dict())
-def event_range_search(request,  *args, **kwargs):
+
+
+def event_range_search(request, *args, **kwargs):
     if request.method == 'POST':
-        res = {'events_in_range':[]}
+        res = {'events_in_range': []}
         # json_data = json.loads('{"ne_lat": 0, "sw_lat":200, "ne_lng": 0, "sw_lng": 200 }')
         range_value = dict(request.POST)
-        keys = ['ne_lat','sw_lat','ne_lng','sw_lng']
+        keys = ['ne_lat', 'sw_lat', 'ne_lng', 'sw_lng']
         for key in keys:
             range_value[key] = float(range_value[key][0])
 
-        for event in Event.get_events_in_range(range_value['ne_lat'], range_value['sw_lat'], range_value['ne_lng'], range_value['sw_lng']):
+        for event in Event.get_events_in_range(
+                range_value['ne_lat'], range_value['sw_lat'], range_value['ne_lng'], range_value['sw_lng']):
             res['events_in_range'].append({
                 'id': event.id,
                 'name': event.name,
@@ -67,5 +70,5 @@ def event_range_search(request,  *args, **kwargs):
                 'latitude': event.latitude,
                 'img': event.get_image_url(),
                 'status': event.get_status()
-                })
+            })
         return JsonResponse(res)
