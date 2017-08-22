9667e90c19b964292b8ae25e49d40c34f7165ddd
Shin Kojima <shin@kojima.org>
2017-08-21 23:37:42 -0700

    [COSME] E501 line too long

--- app/user/social_auth.py
+++ app/user/social_auth.py
@@ -28,14 +28,16 @@ def get_profile_image(strategy, details, response,
 def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
     backend = kwargs.get('backend')
     if user and user.email:
-        return  # The user we're logging in already has their email attribute set
+        # The user we're logging in already has their email attribute set
+        return
     elif is_new and not details.get('email'):
-        # If we're creating a new user, and we can't find the email in the details
-        # we'll attempt to request it from the data returned from our backend strategy
+        # If we're creating a new user, and we can't find the email in the
+        # details.  we'll attempt to request it from the data returned from our
+        # backend strategy
         userEmail = strategy.request_data().get('email')
         if userEmail:
             details['email'] = userEmail
         else:
-            # If there's no email information to be had, we need to ask the user to fill it in
-            # This should redirect us to a view
+            # If there's no email information to be had, we need to ask the
+            # user to fill it in.  This should redirect us to a view
             return redirect('/user/email_required')
