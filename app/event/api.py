9b24b461cfaec6820a3782e0e026031c5cec25b6
Shin Kojima <shin@kojima.org>
2017-08-23 18:30:28 -0700

    [COSME] E203 whitespace before ':'

--- app/event/api.py
+++ app/event/api.py
@@ -34,13 +34,13 @@ def event_filter(request, event_kind, *args, **kwargs):
         res_obj = {'filtered_events': []}
         for event in events[event_kind]()[:10]:
             res_obj['filtered_events'].append({
-                'id' : event.id,
-                'name' : event.name,
-                'start_time' : event.start_time.strftime(DATETIME_FORMAT),
-                'end_time' : event.end_time.strftime(DATETIME_FORMAT),
-                'place' : event.meeting_place,
-                'img' : event.get_image_url(),
-                'status' : event.get_status()
+                'id': event.id,
+                'name': event.name,
+                'start_time': event.start_time.strftime(DATETIME_FORMAT),
+                'end_time': event.end_time.strftime(DATETIME_FORMAT),
+                'place': event.meeting_place,
+                'img': event.get_image_url(),
+                'status': event.get_status()
             })
         return JsonResponse(res_obj)
 
