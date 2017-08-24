3e67e15a22e4994feebd3bfc3497ad5b91e43d4b
Shin Kojima <shin@kojima.org>
2017-08-23 18:29:27 -0700

    [COSME] E203 whitespace before ':'

--- app/event/api.py
+++ app/event/api.py
@@ -25,10 +25,10 @@ def event_filter(request, event_kind, *args, **kwargs):
     events = {'new_events': new_events}
     if not user.is_anonymous():
         events.update({
-            'future_participating_events' : user.get_future_participating_events,
-            'new_region_events' : user.get_new_region_events,
-            'new_tag_events' : user.get_new_tag_events,
-            'all_events' : all_events
+            'future_participating_events': user.get_future_participating_events,
+            'new_region_events': user.get_new_region_events,
+            'new_tag_events': user.get_new_tag_events,
+            'all_events': all_events
         })
     if event_kind in events.keys():
         res_obj = {'filtered_events':[]}
