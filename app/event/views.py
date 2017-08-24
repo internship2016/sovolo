3527eb9b3d0a72bd5de267684d58d4b24ad773fa
Shin Kojima <shin@kojima.org>
2017-08-23 21:05:06 -0700

    [COSME] F401 'django.core.exceptions.ObjectDoesNotExist' imported but unused

--- app/event/views.py
+++ app/event/views.py
@@ -1,6 +1,5 @@
 import os
 
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
