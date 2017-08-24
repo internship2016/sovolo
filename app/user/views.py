ece7e7635ea02178c1be024a930920589c987af8
Shin Kojima <shin@kojima.org>
2017-08-23 17:04:09 -0700

    [COSME] E261 at least two spaces before inline comment

--- app/user/views.py
+++ app/user/views.py
@@ -261,7 +261,7 @@ class UserPostReviewView(FormView):
             to_user = User.objects.get(pk=self.request.GET['to_user_id'])
             form.instance.event_host = True
         else:
-            to_user = User.objects.get(pk=joined_event.host_user.id) # pkを取得 評価対象
+            to_user = User.objects.get(pk=joined_event.host_user.id)  # pkを取得 評価対象
 
 
         ## Validators
