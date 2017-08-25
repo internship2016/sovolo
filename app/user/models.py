31beb8adca6e7ae5dd429a83f289733d60b571fb
shiba0004 <satumakademian@gmail.com>
2017-08-25 10:43:49 -0700

    地区ごとで探せなかった

--- app/user/models.py
+++ app/user/models.py
@@ -390,7 +390,6 @@ class Skill(AbstractBaseModel):
     userskill = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     tag = models.ManyToManyField(Tag, blank=True)
     skilltodo = models.CharField(max_length=200, null=True)
-
     def __str__(self):
         return "Skill #" + str(self.pk) + " in User #" + str(self.userskill_id)
 
