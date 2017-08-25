ea303990bcdef90733ceaeea48e76557113157d3
Shin Kojima <shin@kojima.org>
2017-08-24 18:03:47 -0700

    [COSME] Remove test code

--- app/user/api.py
+++ app/user/api.py
@@ -13,9 +13,6 @@ def get_unreview_list(request, unreview_kind, *args, **kwargs):
     if user.is_anonymous():
         return JsonResponse(res_obj)
 
-    # href="{% url 'user:unreviewed' %}"
-
-    # href="{% url 'user:post_review' %}?event_id={{event_id}}"
     if user.roll == 'helper':
         unreview_list = user.get_past_participated_and_unreviewed_events()  # event
         for event in unreview_list[:back_num]:
@@ -28,7 +25,6 @@ def get_unreview_list(request, unreview_kind, *args, **kwargs):
                 'message': event.name + 'へのレビューをおねがいします。'
             })
 
-    # href="{% url 'user:post_review' %}?event_id={{event_id}}&to_user_id={{helper_id}}"
     else:
         counter = 0
         unreview_list = user.get_zipped_unreviewed_hosted() # zip(event, user_list)
