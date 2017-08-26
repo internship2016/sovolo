05e66f0142c8d046794bfdd8f944816b49affe0d
Shin Kojima <shin@kojima.org>
2017-08-26 16:28:27 -0700

    [BUGFIX] Context instance is deprecated
    https://stackoverflow.com/questions/43787700/django-1-11-typeerror-context-must-be-a-dict-rather-than-context
    
    > In Django 1.8+, the template's render method takes a dictionary for
    > the context parameter. Support for passing a Context instance is
    > deprecated, and gives an error in Django 1.10+.

--- app/base/utils.py
+++ app/base/utils.py
@@ -1,15 +1,14 @@
 from django.template.backends.django import Template
 from django.template.loader import get_template
-from django.template import Context
 from django.core.mail import EmailMessage
 
 
 def send_template_mail(template, context, from_address, to_addresses,
                        bcc_addresses=None):
+
     if not isinstance(template, Template):
         template = get_template(template)
-    if not isinstance(context, Context):
-        context = Context(context)
+
     content = template.render(context)
     subject, body = content.split("\n", 1)
 
