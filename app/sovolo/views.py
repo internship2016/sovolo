c3b1b4ad5bca56a8c7f3e12c90b40a1b31ccc800
flower-pepper <amaretto_milk@Amaretto.local>
2017-08-26 16:44:02 -0700

    トップページの切り替え

--- app/sovolo/views.py
+++ app/sovolo/views.py
@@ -51,4 +51,11 @@ def index_user(request):
     context = {}
 
     context['all_tags'] = Tag.objects.all()
+    context['new_events']  = [event for event in Event.objects.all().order_by('-created') if not event.is_over()][:10]
+
+    prefs = settings.PREFECTURES.items()
+    prefs = sorted(prefs, key=lambda x: x[1][1])
+    prefs = [(k, v[0]) for k, v in prefs]
+    context['prefectures'] = prefs
+
     return render(request, 'top_user.html', context)
