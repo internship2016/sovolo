6c661ac06205508ed35e87433da7f64990cd6c3c
Shin Kojima <shin@kojima.org>
2017-08-26 11:01:17 -0700

    [COSME] E302 expected 2 blank lines, found 1

--- app/event/templatetags/event_tags.py
+++ app/event/templatetags/event_tags.py
@@ -8,10 +8,12 @@ def event_list(context, events, title):
     request = context['request']
     return {'events': events, 'title': title, 'user': request.user}
 
+
 @register.inclusion_tag('event/collapse_event_list.html', takes_context=True)
 def collapse_event_list(context, events, title, event_id):
     request = context['request']
     return {'events': events, 'title': title, 'user': request.user, 'event_id': event_id}
+
 
 @register.simple_tag
 def query_transform(request, **kwargs):
@@ -19,6 +21,7 @@ def query_transform(request, **kwargs):
     for key, value in kwargs.items():
         updated[key] = value
     return updated.urlencode()
+
 
 @register.inclusion_tag('event/comments.html', takes_context=True)
 def comments(context, event):
