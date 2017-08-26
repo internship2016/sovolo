91108bc37773c1716989c8fb31297fe7b80caa2e
Shuto050505 <tsuchiya050505@gmail.com>
2017-08-25 22:09:00 -0700

    move get_unreadlist api -> model

--- app/user/models.py
+++ app/user/models.py
@@ -344,6 +344,55 @@ class User(AbstractBaseModel, AbstractBaseUser):
             num += len(user_list)
         return num
 
+    def get_unreview_list(request):
+        """Get Unreview List.
+        ログインしているユーザーの未レビューの最新５件を返す。
+        """
+        back_num = 5
+
+        res_obj = []
+
+        if user.is_anonymous:
+            return res_obj
+
+        if self.roll == 'helper':
+            unreview_events = self.get_past_participated_and_unreviewed_events()
+            for event in unreview_events[:back_num]:
+                res_obj.append({
+                    'event_id': event.id,
+                    'event_name': event.name,
+                    'event_host': event.host_user,
+                    'event_img': event.get_image_url(),
+                    'message': event.name + 'へのレビューをおねがいします。'
+                })
+
+        else:
+            counter = 0
+
+            # XXX: Bad method name: zip(event, user_list)
+            unreview_events = user.get_zipped_unreviewed_hosted()
+
+            for event, user_list in unreview_events:
+                if counter >= back_num:
+                    break
+
+                for h_user in user_list:
+                    if counter >= back_num:
+                        break
+                    res_obj.append({
+                        'event_id': event.id,
+                        'event_name': event.name,
+                        'event_host': event.host_user,
+                        'event_img': event.get_image_url(),
+                        'helper_name': h_user.username,
+                        'helper_id': h_user.pk,
+                        'helper_img': h_user.get_image_url(),
+                        'message': h_user.username + 'さんへのレビューをおねがいします。'
+                    })
+                    counter += 1
+
+            return res_obj
+
 
 class UserActivation(models.Model):
     user = models.OneToOneField(User)
