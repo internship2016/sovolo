03dff07aa43d9f65b322e1af1df86909605ce66d
Shin Kojima <shin@kojima.org>
2018-01-19 11:06:17 +0900

    Add admin assets

--- app/sovolo/settings.py
+++ app/sovolo/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
