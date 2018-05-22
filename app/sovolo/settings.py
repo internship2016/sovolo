c286eae1317349b8e037e2c20ba7aec411668f6c
hejob.mac <hejob.moyase@gmail.com>
2018-05-22 10:45:02 +0800

    SNS: AuthCanceled exception handler (redirect to home '/')

--- app/sovolo/settings.py
+++ app/sovolo/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'user.auth_exception_middleware.AuthCanceledExceptionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
